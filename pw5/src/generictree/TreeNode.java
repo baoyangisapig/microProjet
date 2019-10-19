954576f1947649ecd88600b0e13da18760a18b67
初一鸣 <cym@meitu.com>
2019-10-19 16:57:28 -0700

    [design] modify the interfaces and codestyle

--- pw5/src/generictree/TreeNode.java
+++ pw5/src/generictree/TreeNode.java
@@ -2,12 +2,12 @@ package generictree;
 
 public interface TreeNode<T> {
   /**
-   * Add the given node as a child to a node in this tree identified by the
-   * predicate. If no node is identified by the predicate the tree remains
-   * unchanged.
+   * Add the given node as a child to a node in this tree identified by the predicate. If no node is
+   * identified by the predicate the tree remains unchanged.
+   *
    * @param child the node that must be added as a child
    * @return the resulting hierarchy starting at this node
    */
-  TreeNode<T> addChild( TreeNode<T> child);
+  void addChild(TreeNode<T> child) throws IllegalArgumentException;
 
 }
