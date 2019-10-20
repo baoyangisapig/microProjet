3be2cf37880df27af69b5c70c65ef985df2c6ece
初一鸣 <cym@meitu.com>
2019-10-19 18:17:30 -0700

    [design] Node modified.

--- pw5/src/generictree/GroupNode.java
+++ pw5/src/generictree/GroupNode.java
@@ -1,27 +1,10 @@
 package generictree;
 
 public class GroupNode<T> extends AbstractTreeNode<T> {
-  private TreeNode<T> left;
-  private TreeNode<T> right;
+  public AbstractTreeNode<T> left;
+  public AbstractTreeNode<T> right;
 
-  private TreeNode<T> getLeft() {
-    return left;
-  }
-
-  private TreeNode<T> getRight() {
-    return right;
-  }
-
-  @Override
-  public void addChild(TreeNode<T> child) throws IllegalArgumentException {
-    if (child == null || (getLeft() != null && getRight() != null)) {
-      throw new IllegalArgumentException("Invalid Expression!");
-    }
-    if (left != null) {
-      left = child;
-    } else {
-      right = child;
-    }
-    return;
+  public GroupNode(T value) {
+    super(value);
   }
 }
